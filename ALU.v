0d77c56587c9e557e163ab1fe849186ac8cf5442
rkhoury18 <84289937+rkhoury18@users.noreply.github.com>
2021-11-29 16:12:33 +0000

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,10 +1,9 @@
 module ALU(
     input logic [31:0] instruction,
-    input logic [31:0] Rmdata,
     input logic [31:0] Rtdata,
     input logic [31:0] Rsdata,
     output logic [31:0] datalo,
-    output logic [31:0]  datahi
+    output logic [31:0] datahi
 
 );
  logic [5:0] type;
@@ -15,24 +14,44 @@ module ALU(
  logic [15:0] immediate;
  logic [4:0] Rmadd;
  logic [5:0] op;
- logic[31:0] addi;
+ logic [31:0] addi;
  logic [31:0] add;
- logic[31:0] bitand;
- logic[31:0] bitandi;
- logic[31:0] divlo;
- logic[31:0] divhi;
- logic[31:0] divulo;
- logic[31:0] divuhi;
+ logic [31:0] bitand;
+ logic [31:0] bitandi;
+ logic [31:0] divlo;
+ logic [31:0] divhi;
+ logic [31:0] divulo;
+ logic [31:0] divuhi;
+ logic [63:0] mult;
+ logic [63:0] multu;
+ logic [31:0] bitor;
+ logic [31:0] bitori;
+ logic [31:0] sll;
+ logic [31:0] sllv;
+ logic [31:0] sra;
+ logic [31:0] srav;
+ logic [31:0] srl;
+ logic [31:0] srlv;
+ logic [31:0] subu;
+ logic [31:0] exor;
+ logic [31:0] exori;
+ logic [31:0] slt;
+ logic [31:0] slti;
+ logic [31:0] sltui;
+ logic [31:0] sltu;
+
+
 
  assign type = instruction[31:26];
  assign immediate = instruction[15:0];
+ assign sa= instruction[10:6];
 
 
 always comb begin
-    addiout=Rtdata + immediate;
-    addout=Rtdata + Rmdata;
-    bitand= Rtdata & Rmdata;
-    bitandi= Rtdata & immediate;
+    addiout=Rsdata + immediate;
+    addout=Rtdata + Rsdata;
+    bitand= Rtdata & Rsdata;
+    bitandi= Rsdata & immediate;
     divuhi= Rsdata/Rtdata;
     divulo=Rsdata%Rtdata;
 
@@ -40,17 +59,17 @@ always comb begin
         temp1= ~(Rsdata)+1;
         temp2= ~(Rtdata)+1;
         divlo= temp1/temp2;
-        divhi= ~(temp1 % temp2)+1
+        divhi= (temp1 % temp2)*(-1);
 
     end
     else if (Rsdata[31]==1)begin
         temp1= ~(Rsdata)+1;
-        divlo=~(temp1/Rtdata)+1;
-        divhi=~(temp1% Rtdata)+1;
+        divlo=(temp1/Rtdata)*(-1);
+        divhi=(temp1% Rtdata)*(-1);
     end
     else if(Rtdata[31]==1)begin
         temp2= ~(Rtdata) +1;
-        divlo=~(Rsdata/temp2) +1;
+        divlo=(Rsdata/temp2)*(-1);
         divhi= Rsdata%temp2;
     end
     else begin
@@ -58,9 +77,98 @@ always comb begin
         divhi= Rsdata%Rtdata;
     end
 
+    multu= Rsdata*Rddata;
+    
+    if ((Rsdata[31]==1) && (Rtdata[31]==1)) begin
+        temp1= ~(Rsdata)+1;
+        temp2= ~(Rtdata)+1;
+        mult=temp1*temp2;
+
+    end
+    else if (Rsdata[31]==1)begin
+        temp1= ~(Rsdata)+1;
+        mult= (temp1*Rtdata)*(-1);
+        
+    end
+    else if(Rtdata[31]==1)begin
+        temp2= ~(Rtdata) +1;
+        mult=(temp2*Rsdata)*(-1);
+    end
+    else begin
+        mult=Rsdata*Rtdata;
+    end
+
+    bitor= Rtdata | Rsdata;
+    bitori= Rsdata | immediate;
+    sll= Rtdata<<sa;
+    sllv= Rtdata<<Rsdata;
+    subu= Rsdata-Rtdata;
+    exor= Rsdata^Rtdata;
+    exori=Rsdata^imediade;
+    sra=Rtdata>>>sa;
+    srav=Rtdata>>>Rsdata;
+    srl=Rtdata>>sa;
+    srlv=Rtdata>>Rsdata;
+
+    if ((Rtdata[31]==1) && (Rsdata[31]==1)) begin
+        temp1=~(Rtdata) +1;
+        temp2=~(Rsdata) +1;
+        if(temp1>temp2)begin
+            slt=0;
+        end
+        else begin
+            slt=1
+        end
+
+    end
+    else if ((Rtdata[31]==1))begin
+        slt=0;
+    end
+    else if (Rsdata[31]==1) begin
+        slt=1;
+    end
+    else begin
+        if (Rsdata<Rtdata) begin
+            slt=1;
+        end
+        else begin
+            slt=0;
+        end
+    end
 
 
+    if ((immediate[31]==1) && (Rsdata[31]==1)) begin
+        temp1=~(immediate) +1;
+        temp2=~(Rsdata) +1;
+        if(temp1>temp2) begin
+            slti=0
+        end
+        else begin
+            slti=1;
+        end
 
+    end
+    else if (immediate[31]==1))begin
+        slti=0;
+    end
+    else if (Rsdata[31]==1) begin
+        slti=1;
+    end
+    else begin
+        if (Rsdata<immediate) begin
+            slti=1;
+        end
+        else begin
+            slti=0;
+        end
+    end
 
+    if (Rsdata<Rtdata)begin
+        sltu=1;
+    end
+    else begin
+        sltu=0;
+    end
 
 end
+endmodule
