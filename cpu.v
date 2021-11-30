4cd0ddabbbec433209d2d559f350284800a6cfc4
Nikhil-Narayanan <nikhilnar02@gmail.com>
2021-11-30 01:43:32 +0000

    created ALU control block
    also fleshed out more of CPU w Abdal

--- cpu.v
+++ cpu.v
@@ -5,7 +5,10 @@ module control_path(
     output logic[31:0] register_v0,
     );
 
-    logic[5:0] opcode;
+    logic[31:0] instruction;
+    logic[5:0] Opcode;
+    logic[5:0] funct;
+
 
     initial begin
         state = 0;
@@ -17,7 +20,7 @@ module control_path(
         EXEC = 1'b1;
     } state;
 
-    typedef enum logic[1:0]{
+
 
 
 //implements state machine 
@@ -32,12 +35,18 @@ module control_path(
 
     end
 
+    assign funct = instruction[5:0];
+    assign Opcode = instruction[31:26];
     assign JType = (Opcode[5:1] == 5'b00001);
-    assign IType;
+    assign RType = (Opcode = 6'b000000);
+    assign IType = (!JType && !RType);
+    assign JUMPType = ((JType) || (RType ==1) && (funct == 6'b001000) || (RType == 1) && (funct == 6'b001001));
+    assign BRANCHType = ((Opcode == 6'b000100) || ((opcode == 6'b000001) && (rt == 5'b00001)) || ((opcode == 6'b000001) && (rt == 5'b10001)) || ((opcode == 6'b000111) && (rt == 5'b00000)) || ((opcode == 6'b000110) && (rt == 5'b00000)) || ((opcode == 6'b000001) && (rt == 5'b00000)) || ((opcode == 6'b000001) && (rt == 5'b10000)) || (opcode == 6'b000101));
     assign PCEn = (state == EXEC);
     assign IREn = (state == FETCH);
     assign AddrSrc = (state == FETCH);
     assign PCSrc1 = ((JumpAndBranchBool) == Jump || (JumpAndBranchBool == Branch));
-    assign PCSrc2 =  
+    assign PCSrc2 = ((RType ==1) && (funct == 6'b001000) || (RType == 1) && (funct == 6'b001001));
+    assign PCSrc3 = (JType);
 
 endmodule
