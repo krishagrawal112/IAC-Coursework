b8a4337490151003648501a803a878ee295a3dfe
krishagrawal112 <krishagrawal112@gmail.com>
2021-12-01 14:28:55 +0000

    a

--- load_store.v
+++ load_store.v
@@ -1,34 +1,57 @@
 module load_store(
 
     input logic clk,
-    input logic [31:0] reg0,
-    input logic [31:0] reg1,
+    input logic[1:0] state,
+    input logic lb,
+    input logic lbu,
+    input logic lh,
+    input logic lhu,
+    input logic lui,
+    input logic lw,
+    input logic lwl,
+    input logic lwr,
+    input logic sb,
+    input logic sh,
+    input logic sw,
+    input logic[4:0] rs;
+    input logic[4:0] rt;
+    input logic[15:0] offset;
+    input logic [31:0] rs_data,
+
     
+    input logic [31:0] mem_readdata,
+    output logic reg_writeenable,
+    output logic [3:0] reg_byteenable,
+    output logic [4:0] reg_addr,
+
+    input logic [31:0] rt_data,
+    output logic mem_writeenable,
+    output logic [3:0] mem_byteenable
+    output logic [31:0] mem_writedata,
+    output logic [31:0] mem_address,
     
-    
-    input logic regfilewriteenable,
-    output logic [3:0] regbyteenable,
-    output logic [4:0] regfileaddr
-    output logic [31:0] regoutput
 
     input logic waitrequest,
-    input logic [31:0] readdata,
-    output logic [31:0] address,
-    output logic write,
-    output logic read,
-    output logic [31:0] writedata,
-    output logic [3:0] byteenable
+    
+    //output logic [31:0] address,
+    //output logic write,
+    //output logic read,
+    //output logic [31:0] mem_writedata,
+    //output logic [3:0] mem_byteenable
 
 );
 
-    logic [5:0] opcode;
-    logic [4:0]
-    logic [4:0]
-    logic [15:0]
- 
-    assign opcode = instruction[31:26];
-
     always_comb begin
+        if (lw == 1) begin
+            if (state == 2'b01)begin
+                
+            end
+            if (state == 2'b
+            reg_writeenable = 1;
+            reg_byteenable = 4'b1111;
+            reg_addr = rt;
+
+        osdend
 
     end
 
