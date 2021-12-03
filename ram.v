6bc4ca1b1dce45c045f3898fea4ad1d1f108db3c
Zsombito <klapper.zsombor@gmail.com>
2021-12-03 18:37:40 +0000

    Ram done?

--- ram.v
+++ ram.v
@@ -1,13 +1,27 @@
 module RAM(
-    
+    input logic[31:0] address,
+    input logic clk,
+    output logic[31:0] read_data,
+    input logic[31:0] write_data,
+    input logic write_enable
 );
 
-reg[7:0] memory [31:0];
+reg[31:0] memory [536870911:0]
 integer i;
 initial begin
+    for(i = 0; i < 536870912; i++) memory[i] = 0;
+/*
     $readmemh("test1.txt", memory);
-    for (i = 0; i < 32; i++) begin
+    for (i = 0; i < 30; i++) begin
         $display("%h: %h", i, memory[i]);
     end
+
+*/
 end
+
+always_ff @(posedge clk)begin
+    if(write_enable) memory[address/4] <= write_data;
+    read_data <= memory[address/4];
+end
+
 endmodule
