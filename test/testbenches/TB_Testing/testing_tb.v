22830a5bed86d8b621f7aa701ae78372a069ecc8
Mineglitch <pavangerow@gmail.com>
2021-12-10 22:08:02 +0000

    Trying to make things work

---
+++ test/testbenches/TB_Testing/testing_tb.v
@@ -0,0 +1,71 @@
+module testing_tb ();
+
+logic clk;
+logic reset;
+logic active;
+logic[31:0] register_v0;
+logic waitrequest;
+logic [31:0] readdata;
+logic [31:0] address;
+logic write;
+logic read;
+logic [31:0] writedata;
+logic [3:0] byteenable;
+
+//RAM
+logic[31:0] shifted_address;
+assign shifted_address = address - 32'hBFC00000;
+reg[31:0] memory [0:1000];
+
+initial begin
+    $dumpfile("result.vcd");
+    $dumpvars(0, testing_tb);
+    $readmemb("ram.txt", memory, 0, 1);
+
+    clk = 0;
+
+    repeat(1000) begin
+        clk = !clk; #10;
+    end
+
+    $fatal(1, "Timeout error");    
+end
+
+initial begin
+    
+    while (active == 1) begin
+        
+        @ (posedge clk) begin
+            if (write) begin
+                memory[shifted_address/4] <= writedata;
+            end
+
+            readdata <= (address == 0) ? 0 : memory[shifted_address/4];
+        end
+    end
+
+    if (register_v0 == 16) begin
+        $display("Test Passed");
+        $finish;
+    end
+    else begin
+        $fatal(1, "Failed.");
+    end
+
+end
+
+mips_cpu_bus m1(
+    .reset(reset),
+    .active(active),
+    .register_v0(register_v0),
+    .waitrequest(waitrequest),
+    .readdata(readdata),
+    .address(address),
+    .write(write),
+    .read(read),
+    .writedata(writedata),
+    .byteenable(byteenable),
+    .clk
+);
+
+endmodule
