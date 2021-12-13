53502e7ab86d71c941f1827cb694493ab55f7353
Zsombito <klapper.zsombor@gmail.com>
2021-12-13 13:20:17 +0000

    Working FIBONACCI test

--- test/testbenches/FIBONACCI_TB/fibonacci.asm
+++ test/testbenches/FIBONACCI_TB/fibonacci.asm
@@ -1,5 +1,5 @@
 addiu $s3 $zero BFC0H
-sll $s3 $s3 0010H 
+sll $s3 $s3 0010H
 addiu $s3 $s3 001CH #Setting s3 to the address of line 8
 addiu $t0 $zero 0010H #T0 is the iteration variable
 addiu $s0 $zero 0001H
@@ -10,7 +10,8 @@ sll $zero $zero 0000H #Branch delay if needed, otherwise just a NULL instruction
 addu $t1 $s0 $s1
 addu $s0 $s1 $zero
 addu $s1 $t1 $zero # setting s0 and s1 to next fibonacci numbers
-sub $t0 $t0 $s2 #making iteration variable smaller by one
+subu $t0 $t0 $s2 #making iteration variable smaller by one
 jr $s3 #jump back to the start of loop
+sll 0 0 0 
 j 0000H #Stop instruction
 addu $v0 $s1 $zero #Saving the results in branch delay slot
