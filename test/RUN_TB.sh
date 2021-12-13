500d18def59f2101bb9b53e2f6e57da9abec287d
Mineglitch <pavangerow@gmail.com>
2021-12-13 20:14:08 +0000

    Modifed RUN_TB.sh
    Should work now in the correct format

--- test/RUN_TB.sh
+++ test/RUN_TB.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# USAGE : ./RUN_TB {TB_FOLDER_NAME} {path_to_rtl} {ECHO_EN}
+# USAGE : ./RUN_TB {TB_FOLDER_NAME} {path_to_rtl}
 
 curr_TB=$1
 rtl_DIRECTORY=$2
-progress_msg=$3
+
+inst=
+
+outcome=""
+error=""
 
 test_bench_DIRECTORY=`pwd`/testbenches/$curr_TB
 
@@ -18,19 +22,21 @@ else
     files=`ls $rtl_DIRECTORY/*.v`
 fi
 
-if [ "$progress_msg" = "1" ] 
-then
-    echo "Compiling: "${curr_TB}
-fi
-
 cd $test_bench_DIRECTORY 
 
-iverilog -g2012 -Wall -s testing_tb -o $test_bench_DIRECTORY/testing_tb_out $test_bench_DIRECTORY/testing_tb.v $files
+iverilog -g2012 -Wall -s testing_tb -o $test_bench_DIRECTORY/testing_tb_out $test_bench_DIRECTORY/testing_tb.v $files >/dev/null
+./testing_tb_out >/dev/null
 
-if [ "$progress_msg" = "1" ] 
+if [ "$?" -eq "1" ]
 then
-    echo "Success!"
-    echo "Running..."
+    error="Timeout"
+    outcome="Fail"
+elif [ "$?" -eq "2" ]
+then
+    error="Incorrect Output Value"
+    outcome="Fail"
+else 
+    outcome="Pass"
 fi
 
-./testing_tb_out
+echo ${curr_TB}" "${curr_TB%_*}" "${outcome}" "${error}
